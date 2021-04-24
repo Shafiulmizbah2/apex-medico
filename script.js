95c7f41dd890b78dc228ba2dfc834bda2ee32666
sazid-hassan <sazid.khan.hassan@gmail.com>
2021-04-25 00:53:11 +0600

    sazid

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-const getID = id => document.getElementById(id);
+let form = document.querySelector('#locationBox');
+
+let myBtn = document.querySelector('#addLocation');
+
+
+const myHTML = `<div style="padding-top:40px; padding-left: 1px; border : 0.5px solid;">
+<div class="form-group" >
+    <select class="form-control" id="exampleFormControlSelect1" style="border: none; border-bottom: 2px solid #617ebd;">
+    <option >Please Select Medical Record Type</option>  
+    <option>GP Records</option>
+      <option>Hospital Records</option>
+      <option>X-rays</option>
+      <option>Physiotherapy Records</option>
+      <option>Walk in Records</option>
+      <option>Dental Records</option>
+      <option>Counselling Records</option>
+      <option>Employment Records</option>
+      <option>Miscellaneous Records</option>
+    </select>
+  </div>
+  
+  <input type="text" class="form-control" placeholder="Location Name" style = "margin-top:30px; border: none; border-bottom: 2px solid #617ebd;" required>
+
+  <div class="form-group">
+      <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid #617ebd;" placeholder="Address" rows="3" required></textarea>
+  </div>
+
+  <input type="number" class="form-control" placeholder="Post Code" style = "margin-top:30px; border: none; border-bottom: 2px solid #617ebd;" required>
+
+  <div class="form-group">
+      <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid #617ebd;" placeholder="Additional Notes" rows="3" required></textarea>
+  </div>
 
 
 
-getID('addLocation').addEventListener('click', () => {
+</div>`;
 
-    const myHTML = `<div id="locationBox">
-    <div class="form-group">
-        <select class="form-control" id="exampleFormControlSelect1" style="border: none; border-bottom: 2px solid;">
-          <option>GP Records</option>
-          <option>Hospital Records</option>
-          <option>X-rays</option>
-          <option>Physiotherapy Records</option>
-          <option>Walk in Records</option>
-          <option>Dental Records</option>
-          <option>Counselling Records</option>
-          <option>Employment Records</option>
-          <option>Miscellaneous Records</option>
-        </select>
-      </div>
-      
-      <input type="text" class="form-control" placeholder="Location Name" style = "margin-top:30px; border: none; border-bottom: 2px solid;" required>
+const clicked = console.log(2)
 
-      <div class="form-group">
-          <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid;" placeholder="Address" rows="3" required></textarea>
-      </div>
+const addBtn = myBtn.addEventListener('click', () => {
+  console.log(Array.from(myBtn).length);
+  console.log('working');
+  form.insertAdjacentHTML("beforeend", myHTML);
 
-      <input type="number" class="form-control" placeholder="Post Code" style = "margin-top:30px; border: none; border-bottom: 2px solid;" required>
-
-      <div class="form-group">
-          <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid;" placeholder="Additional Notes" rows="3" required></textarea>
-      </div>
-
-      <button type="button" class="btn btn-primary" id="addLocation" style="background-color : #27b8d4; float: right; margin : 10px;">Add Location</button>
-
-
-</div>`
-
-    var divC = getID('locationBox');
-    divC.insertAdjacentHTML("beforeend", myHTML);
-    console.log("hello")
 })
+
+
+
+// getID('addLocation').addEventListener('click', () => {
+
+
+
+
+//     var divC = getID('locationBox');
+//     divC.insertAdjacentHTML("beforeend", myHTML);
+//     console.log("hello")
+// })
+
