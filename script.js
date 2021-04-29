6916082e9d6ec9fbfa689abf1f2b54ce55b742db
Shafiul Azim <shafiulmizbah2@gmail.com>
2021-04-29 20:48:20 +0600

    FInal review

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let form = document.querySelector('#locationBox');
 let myBtn = document.querySelector('#addLocation');
 
 
+<<<<<<< HEAD
+const myHTML = `<div style="padding-top:40px; ">
+<div class="form-group" >
+    <select class="form-control" id="exampleFormControlSelect1" style="border: none; border-bottom: 2px solid #00b1ff; font-size: 20px; opacity: 0.8;">
+=======
 const myHTML = `<div style="padding-top:40px; padding-left: 1px; border : 0.5px solid;">
 <div class="form-group" >
     <select class="form-control" id="exampleFormControlSelect1" style="border: none; border-bottom: 2px solid #617ebd;">
+>>>>>>> 95c7f41dd890b78dc228ba2dfc834bda2ee32666
     <option >Please Select Medical Record Type</option>  
     <option>GP Records</option>
       <option>Hospital Records</option>
@@ -19,6 +25,39 @@ const myHTML = `<div style="padding-top:40px; padding-left: 1px; border : 0.5px 
     </select>
   </div>
   
+<<<<<<< HEAD
+  <input type="text" class="form-control" placeholder="Location Name" style = "margin-top:30px; border: none; border-bottom: 2px solid #00b1ff; opacity: 0.6;" required>
+
+  <div class="form-group">
+      <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid #00b1ff; opacity: 0.6;" placeholder="Address" rows="3" required></textarea>
+  </div>
+
+  <div class="row">
+            <div class="col-md-6">
+              <p> </p>
+            </div>
+          <div class="col-md-6">
+            <div style="margin-right: px;">
+              <input type="number" class="form-control" placeholder="Postcode" style = "margin-top:30px; border: none; border-bottom: 2px solid #00b1ff; margin-right: 30px; opacity: 0.6;" required>
+            </div>
+         
+          </div>
+        </div>
+
+  <div class="form-group">
+      <textarea class="form-control" id="exampleFormControlTextarea1" style = "margin-top:30px; border: none; border-bottom: 2px solid #00b1ff; opacity: 0.6;" placeholder="Additional Notes" rows="3" required></textarea>
+  </div>
+  
+  <div class="form-group">
+  <button class="btn btn-info" style= "background-color: #00b1ff;" >Delete</button>
+  </div>
+
+
+</div>`;
+
+const clicked = console.log(2)
+
+=======
   <input type="text" class="form-control" placeholder="Location Name" style = "margin-top:30px; border: none; border-bottom: 2px solid #617ebd;" required>
 
   <div class="form-group">
@@ -37,6 +76,7 @@ const myHTML = `<div style="padding-top:40px; padding-left: 1px; border : 0.5px 
 
 const clicked = console.log(2)
 
+>>>>>>> 95c7f41dd890b78dc228ba2dfc834bda2ee32666
 const addBtn = myBtn.addEventListener('click', () => {
   console.log(Array.from(myBtn).length);
   console.log('working');
